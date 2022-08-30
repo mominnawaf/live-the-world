9124257282359b05b26c910cd2774ec29bd068f3
mominnawaf <mominnawaf@gmail.com>
2022-08-30 22:22:32 +0530

    feat: TopBar Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
+import TopBar from './features/TopBar/TopBar';
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
     </div>
   );
 }
---
+++ src/features/TopBar/TopBar.tsx
@@ -0,0 +1,28 @@
+import {
+    AppBar,
+    Toolbar,
+    Avatar,
+} from '@mui/material'
+import Logo from '../../assets/logo.svg'
+import  styles from'./TopBar.module.css'
+
+function TopBar() {
+  return (
+    <AppBar title="TopBar" position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: 'white', boxShadow: `0px 0px 15px rgba(2, 27, 121, 0.2)` }}>
+    <Toolbar variant='dense'>
+      <div className={styles.toolBar}>
+        <div className={styles.toolBarLeft}>
+          <div className={styles.logoDiv}>
+            <img src={Logo} alt="logo" className={styles.logoImg} />
+          </div>
+        </div>
+        <div className={styles.ToolBarRight}>
+          <Avatar sx={{ width: 46, height: 46, backgroundColor: 'rgb(252 107 67)' }} />
+        </div>
+      </div>
+    </Toolbar>
+  </AppBar>
+  )
+}
+
+export default TopBar
